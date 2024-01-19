000c2c8a76cbee1323a1140bcfdae994fcaa11c8
brigid <brigidsang2899@gmail.com>
2024-01-19 19:21:42 +0300

    Trial commit

---
+++ Week 1/basic_data_types.py
@@ -0,0 +1,17 @@
+#Numeric >  float ,integer and complex numbers
+#Float
+points= 32.5;
+print(points);
+print(type(points));
+position=3;
+print(position);
+print(type(position));
+#Complex Numbers
+#num1=10+10i;
+#print(num1);
+#print(type(num1));
+#Sequence > Strings, list and tuples
+my
+#Dictionary
+#Boolean
+#Sey
---
+++ Week 1/variable.py
@@ -0,0 +1,20 @@
+#Declaration and assigning
+#Integer
+x=10;
+print(x);
+print(type(x));
+#Reassigning
+x=20;
+print(x)
+print(type(x));
+#String
+myName="Brigid";
+print(myName);
+print(type(myName));
+#Float
+rating=4.3;
+print(rating);
+print(type(rating));
+#Delete a variable using del
+#del rating;
+print(rating);
